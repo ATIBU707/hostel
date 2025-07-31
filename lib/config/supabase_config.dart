da27245da00d1ca1a5632cdaef981b0561189b47
ATIBU707 <wasswaatibu@gmail.com>
2025-07-31 14:30:42 +0300

    main.dart updated

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,7 +1,7 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String supabaseUrl = 'https://hnvjhtkghflprqjlpqtr.supabase.co';
+  static const String supabaseUrl = 'hnvjhtkghflprqjlpqtr.supabase.co';
   static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudmpodGtnaGZscHJxamxwcXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5MDU0MDgsImV4cCI6MjAzNzQ4MTQwOH0.hBq29C0cKqB0pvv3n_Gk2q-2v3f12h3Kj2p3i_1Rj_E';
 
   static Future<void> initialize() async {
